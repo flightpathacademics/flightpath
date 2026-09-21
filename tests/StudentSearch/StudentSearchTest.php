c66e7ae71e84280989637acb64ebd26cf6ae70e6
Richard Peacock <swampopus@gmail.com>
2026-09-21 08:36:43 -0500

    Github testing

---
+++ tests/StudentSearch/StudentSearchTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+
+require_once __DIR__ . '/../bootstrap.php';
+
+class StudentSearchTest extends TestCase {
+
+  protected function setUp(): void {
+    global $user;
+
+    $user = new stdClass();
+    $user->id = 1;
+    $user->cwid = 1;
+    $user->name = 'admin';
+    $user->school_id = 0;
+  }
+
+  #[DataProvider('studentSearchTermsProvider')]
+  public function testStudentCanBeFoundBySearchTerm(string $search): void {
+    $_REQUEST['search_for'] = $search;
+
+    $form = student_search_search_form();
+    $results = $form['adv_array']['value'];
+
+    $this->assertIsArray($results);
+    $this->assertArrayHasKey('999999999', $results);
+    $this->assertSame('Test', $results['999999999']['first_name']);
+    $this->assertSame('Student', $results['999999999']['last_name']);
+
+    $html = $form['mark_search_results']['value'];
+
+    $this->assertStringContainsString('Test', $html);
+    $this->assertStringContainsString('Student', $html);
+    $this->assertStringContainsString('999999999', $html);
+  }
+
+  public static function studentSearchTermsProvider(): array {
+    return [
+      'first name' => ['Test'],
+      'last name' => ['Student'],
+      'full name' => ['Test Student'],
+      'CWID' => ['999999999'],
+    ];
+  }
+}
