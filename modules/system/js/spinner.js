c729a51807081232e7615cf9b464f55bb1b1a189
Richard Peacock <swampopus@gmail.com>
2026-09-24 13:35:54 -0500

    Fixed bug where "spinner" buttons remained disabled after form submission, then clicking Back   in the browser.

--- modules/system/js/spinner.js
+++ modules/system/js/spinner.js
@@ -1,13 +1,21 @@
 // This js file deals specifically with "spinner" gifs showing or hiding when we click a button.
 $(document).ready(function() {
-      
+
     // Attach behavior to any element with the "show-spinner" class.
     $(".show-spinner").click(function() {
       var name = $(this).attr("name");
       if (name) {
         $(this).addClass("disable-element");
-        $(".loading-spinner-" + name).show();  
+        $(".loading-spinner-" + name).show();
       }
+    });
+
+
+    // If we are returning from having clicked "back", we don't want
+    // a previously disabled button to still be disabled/spinner showing.
+    $(window).on('pageshow', function() {
+      $(".show-spinner").removeClass('disable-element');
+      $(".loading-spinner").hide();
     });
 
 
